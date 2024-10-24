f104684259e89045ff817d7b690fd01d9ad810f8
Viktor Sitnikov <sitnikoff_victor_97@mail.ru>
2024-10-24 18:18:18 +0300

    change Main.java

--- test/src/main/java/org/example/Main.java
+++ test/src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
     }
 
     public static int multiply(int a, int b) {
-        return a * b;
+        return a + b;
     }
     public static int add(int a, int b) {
         return a - b;
